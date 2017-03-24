c3cd07655b2ea03eab3b6d905ffe6f7b48054e97
Alex <alexjcampbe@gmail.com>
2017-03-23 22:16:58 -0700

    poop

--- js/registration-form.js
+++ js/registration-form.js
@@ -1,10 +1,10 @@
 //function to help referencing
-function $(id){ 
-	var element = document.getElementById(id); 
+function $(id){
+	var element = document.getElementById(id);
 	if( element == null ){
-		alert( 'Programmer error: ' + id + 'does not exist.'); 
-	}	
-		return element; 
+		alert( 'Programmer error: ' + id + 'does not exist.');
+	}
+	return element;
 }
 
 /*Login & signup page Validation*/
@@ -12,63 +12,63 @@ function $(id){
 /***Login***/
 //Username validation
 function testNameBlankL(id){
-		if($(id).value.trim().length != 0){
-			return true;
-		}else{
-			return false;
-		}
+	if($(id).value.trim().length != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testNameValidL(id){
-		var nameFormat = /^[\w]{1,15}$/;
-		if($(id).value.trim().match(nameFormat)){
-			return true;
-		}else{
-			return false;
-		}
+	var nameFormat = /^[\w]{1,15}$/;
+	if($(id).value.trim().match(nameFormat)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnNameValidL(id){
-		if(!testNameBlankL(id)){
-			$("alertUserNameL").style.visibility = "visible";
-			return false;
-		}else if(!testNameValidL(id)){
-			$("alertUserNameL").innerHTML = "1-15 letters,only A-Z,a-z,0-9 & \"_\"";
-			$("alertUserNameL").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertUserNameL").style.visibility = "hidden";
-			return true;
-			}
+	if(!testNameBlankL(id)){
+		$("alertUserNameL").style.visibility = "visible";
+		return false;
+	}else if(!testNameValidL(id)){
+		$("alertUserNameL").innerHTML = "1-15 letters,only A-Z,a-z,0-9 & \"_\"";
+		$("alertUserNameL").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertUserNameL").style.visibility = "hidden";
+		return true;
+	}
 }
 //Password validation
 function testPasswordBlankL(id) {
-		var password = $(id).value.trim();
-		if(password != 0){
-			return true;
-		}else{
-			return false;
-		}
+	var password = $(id).value.trim();
+	if(password != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testPasswordValidL(id) {
-		var password = $(id).value.trim();
-		var passwordTest = /^[\w]{6,10}$/;
-		if(password.match(passwordTest)){
-			return true;
-		}else{
-			return false;
-		}
+	var password = $(id).value.trim();
+	var passwordTest = /^[\w]{6,10}$/;
+	if(password.match(passwordTest)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnPasswordValidL(id){
 	if(!testPasswordBlankL(id)){
-			$("alertPasswordL").style.visibility = "visible";
-			return false;
-		}else if(!testPasswordValidL(id)){
-			$("alertPasswordL").innerHTML = "6-10letters,only A-Z,a-z,0-9 & \"_\"";
-			$("alertPasswordL").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertPasswordL").style.visibility = "hidden";
-			return true;
-		}
+		$("alertPasswordL").style.visibility = "visible";
+		return false;
+	}else if(!testPasswordValidL(id)){
+		$("alertPasswordL").innerHTML = "6-10letters,only A-Z,a-z,0-9 & \"_\"";
+		$("alertPasswordL").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertPasswordL").style.visibility = "hidden";
+		return true;
+	}
 }
 //Validation Check all values when user submit:for login page
 function loginSubmitValid() {
@@ -79,7 +79,7 @@ function loginSubmitValid() {
 		ret = false;
 	}
 	return ret;
-	
+
 }
 function SubmitLogin() {
 	if(!loginSubmitValid()){
@@ -88,104 +88,104 @@ function SubmitLogin() {
 		return true;
 		document.loginForm.submit();
 	}
-	
+
 }
 
 /***Signup***/
 
 //Username validation
 function testNameBlank(id){
-		if($(id).value.trim().length != 0){
-			return true;
-		}else{
-			return false;
-		}
+	if($(id).value.trim().length != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testNameValid(id){
-		var nameTest = /^[\w]{1,15}$/;
-		if($(id).value.trim().match(nameTest)){
-			return true;
-		}else{
-			return false;
-		}
+	var nameTest = /^[\w]{1,15}$/;
+	if($(id).value.trim().match(nameTest)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnNameValid(id){
-		if(!testNameBlank(id)){
-			$("alertUserName").style.visibility = "visible";
-			return false;
-		}else if(!testNameValid(id)){
-			$("alertUserName").innerHTML = "1-15 letters,only A-Z,a-z,0-9 & \"_\"";
-			$("alertUserName").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertUserName").style.visibility = "hidden";
-			return true;
-			}
+	if(!testNameBlank(id)){
+		$("alertUserName").style.visibility = "visible";
+		return false;
+	}else if(!testNameValid(id)){
+		$("alertUserName").innerHTML = "1-15 letters,only A-Z,a-z,0-9 & \"_\"";
+		$("alertUserName").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertUserName").style.visibility = "hidden";
+		return true;
+	}
 }
 
 //E-mail validation
 function testEmailBlank(id) {
-		var email = $(id).value.trim();
-		if(email != 0){
-			return true;
-		}else{
-			return false;
-		}
+	var email = $(id).value.trim();
+	if(email != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testEmailValid(id) {
-		var email = $(id).value.trim();
-		var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if( email.match(emailFormat)){
-			return true;
-		}else{
-			return false;
-		}
+	var email = $(id).value.trim();
+	var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if( email.match(emailFormat)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnEmailValid(id) {
-		if(!testEmailBlank(id)){
-			$("alertEmail").style.visibility = "visible";
-			return false;
-		}else if(!testEmailValid(id)){
-			$("alertEmail").innerHTML = "Invalid e-mail adress";
-			$("alertEmail").style.visibility = "visible";
-			$(id).className = 'invalidForm';
-			return false;
-		}else{
-			$("alertEmail").style.visibility = "hidden";
-			return true;
-		}
+	if(!testEmailBlank(id)){
+		$("alertEmail").style.visibility = "visible";
+		return false;
+	}else if(!testEmailValid(id)){
+		$("alertEmail").innerHTML = "Invalid e-mail adress";
+		$("alertEmail").style.visibility = "visible";
+		$(id).className = 'invalidForm';
+		return false;
+	}else{
+		$("alertEmail").style.visibility = "hidden";
+		return true;
+	}
 }
 
 //Password validation
 function testPasswordBlank(id) {
-		var password = $(id).value.trim();
-		if(password != 0){
-			return true;
-		}else{
-			return false;
-		}
+	var password = $(id).value.trim();
+	if(password != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testPasswordValid(id) {
-		var password = $(id).value.trim();
-		var passwordTest = /^[\w]{6,10}$/;
-		if(password.match(passwordTest)){
-			return true;
-		}else{
-			return false;
-		}
+	var password = $(id).value.trim();
+	var passwordTest = /^[\w]{6,10}$/;
+	if(password.match(passwordTest)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnPasswordValid(id){
 	if(!testPasswordBlank(id)){
-			$("alertPassword").style.visibility = "visible";
-			return false;
-		}else if(!testPasswordValid(id)){
-			$("alertPassword").innerHTML = "6-10 letters, only use A-Z,a-z,0-9 & \"_\"";
-			$("alertPassword").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertPassword").style.visibility = "hidden";
-			return true;
-		}
+		$("alertPassword").style.visibility = "visible";
+		return false;
+	}else if(!testPasswordValid(id)){
+		$("alertPassword").innerHTML = "6-10 letters, only use A-Z,a-z,0-9 & \"_\"";
+		$("alertPassword").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertPassword").style.visibility = "hidden";
+		return true;
+	}
 }
 
 //Validation Check all values when user submit :for sign up page
@@ -194,16 +194,16 @@ function testSubmitValid() {
 	if(!(warnNameValid('reguser'))){
 		ret = false;
 	}
-	
-	if(!(warnEmailValid('email'))){ 
+
+	if(!(warnEmailValid('email'))){
 		ret = false;
 	}
-	
+
 	if(!(warnPasswordValid('regpassword'))){
 		ret = false;
 	}
 	return ret;
-	
+
 	if(!(warnPasswordConValid('passwordCon'))){
 		ret = false;
 	}
@@ -221,111 +221,115 @@ function submitSignup(){
 /***Contact form***/
 //FristName validation
 function testFirstNameBlank(id){
-		if($(id).value.trim().length != 0){
-			return true;
-		}else{
-			return false;
-		}
+	if($(id).value.trim().length != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testFirstNameValid(id){
-		var nameFormat = /^[\w]{1,10}$/;
-		if($(id).value.trim().match(nameFormat)){
-			return true;
-		}else{
-			return false;
-		}
+	var regexNum = /\d/g;
+	var a = $(id).value;
+	var c = regexNum.test(a);
+	if(c)
+	return false;
+	else{
+		return true;
+	}
 }
 function warnFirstNameValid(id){
-		if(!testFirstNameBlank(id)){
-			$("alertFirstName").style.visibility = "visible";
-			return false;
-		}else if(!testFirstNameValid(id)){
-			$("alertFirstName").innerHTML = "1-10 letters,only A-Z,a-z,0-9 & \"_\"";
-			$("alertFirstName").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertFirstName").style.visibility = "hidden";
-			$(id).className = '';
-			return true;
-			}
+	if(!testFirstNameBlank(id)){
+		$("alertFirstName").style.visibility = "visible";
+		return false;
+	}else if(!testFirstNameValid(id)){
+		$("alertFirstName").innerHTML = "1-10 letters,only A-Z,a-z";
+		$("alertFirstName").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertFirstName").style.visibility = "hidden";
+		$(id).className = '';
+		return true;
+	}
 }
 //LastName validation
 function testLastNameBlank(id){
-		if($(id).value.trim().length != 0){
-			return true;
-		}else{
-			return false;
-		}
+	if($(id).value.trim().length != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testLastNameValid(id){
-		var nameFormat = /^[\w]{1,10}$/;
-		if($(id).value.trim().match(nameFormat)){
-			return true;
-		}else{
-			return false;
-		}
+	var regexNum = /\d/g;
+	var a = $(id).value;
+	var c = regexNum.test(a);
+	if(c)
+	return false;
+	else{
+		return true;
+	}
 }
 function warnLastNameValid(id){
-		if(!testLastNameBlank(id)){
-			$("alertLastName").style.visibility = "visible";
-			return false;
-		}else if(!testLastNameValid(id)){
-			$("alertLastName").innerHTML = "1-10 letters,only A-Z,a-z,0-9 & \"_\"";
-			$("alertLastName").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertLastName").style.visibility = "hidden";
-			return true;
-			}
+	if(!testLastNameBlank(id)){
+		$("alertLastName").style.visibility = "visible";
+		return false;
+	}else if(!testLastNameValid(id)){
+		$("alertLastName").innerHTML = "1-10 letters,only A-Z,a-z";
+		$("alertLastName").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertLastName").style.visibility = "hidden";
+		return true;
+	}
 }
 //E-mail validation
 function testEmailConBlank(id) {
-		var email = $(id).value.trim();
-		if(email != 0){
-			return true;
-		}else{
-			return false;
-		}
+	var email = $(id).value.trim();
+	if(email != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function testEmailConValid(id) {
-		var email = $(id).value.trim();
-		var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-		if( email.match(emailFormat)){
-			return true;
-		}else{
-			return false;
-		}
+	var email = $(id).value.trim();
+	var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if( email.match(emailFormat)){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnEmailConValid(id) {
-		if(!testEmailConBlank(id)){
-			$("alertConEmail").style.visibility = "visible";
-			return false;
-		}else if(!testEmailConValid(id)){
-			$("alertConEmail").innerHTML = "Invalid e-mail adress";
-			$("alertConEmail").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertConEmail").style.visibility = "hidden";
-			return true;
-		}
+	if(!testEmailConBlank(id)){
+		$("alertConEmail").style.visibility = "visible";
+		return false;
+	}else if(!testEmailConValid(id)){
+		$("alertConEmail").innerHTML = "Invalid e-mail adress";
+		$("alertConEmail").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertConEmail").style.visibility = "hidden";
+		return true;
+	}
 }
 //Textarea validation
 function testTextareaBlank(id) {
-		var textArea = $(id).value.trim();
-		if(textArea != 0){
-			return true;
-		}else{
-			return false;
-		}
+	var textArea = $(id).value.trim();
+	if(textArea != 0){
+		return true;
+	}else{
+		return false;
+	}
 }
 function warnTextareaBlank(id) {
-		if(!testTextareaBlank(id)){
-			$("alertTextarea").style.visibility = "visible";
-			return false;
-		}else{
-			$("alertTextarea").style.visibility = "hidden";
-			return true;
-		}
+	if(!testTextareaBlank(id)){
+		$("alertTextarea").style.visibility = "visible";
+		return false;
+	}else{
+		$("alertTextarea").style.visibility = "hidden";
+		return true;
+	}
 }
 
 //Validation Check all values when user submit :for sign up page
@@ -334,14 +338,14 @@ function testSubmitConValid() {
 	if(!(warnFirstNameValid('fname'))){
 		ret = false;
 	}
-	
+
 	if(!(warnLastNameValid('lname'))){
 		ret = false;
 	}
-	
-	if(!(warnEmailConValid('emailCon'))){ 
+
+	if(!(warnEmailConValid('emailCon'))){
 		ret = false;
-	
+
 	}
 	if(!(warnTextareaBlank('comment'))){
 		ret = false;
